7c88b4c67b0cdbf7ba2e1fc12b03e5f88b259bf4
Artem Safin <artemsafin.webdev@gmail.com>
2025-09-10 02:31:17 +0300

    По анимациям

--- src/js/homeLoading.ts
+++ src/js/homeLoading.ts
@@ -8,15 +8,26 @@ export default function homeLoading() {
     delay: 0.5,
   });
 
-  tl.from(".intro__heading", {
-    autoAlpha: 0,
-    duration: 1,
-    y: 60,
-  });
-  tl.from(
+  tl.fromTo(
+    ".intro__heading",
+    {
+      autoAlpha: 0,
+      y: 60,
+    },
+    {
+      autoAlpha: 1,
+      duration: 1,
+      y: 0,
+    }
+  );
+  tl.fromTo(
     ".intro__slider",
     {
       autoAlpha: 0,
+      duration: 1,
+    },
+    {
+      autoAlpha: 1,
       duration: 1,
     },
     "-=0.5"
--- src/js/partnersLoading.ts
+++ src/js/partnersLoading.ts
@@ -7,23 +7,37 @@ export default function partnersLoading() {
   const tl = gsap.timeline({
     delay: 0.5,
   });
-  tl.from(".partners-intro__breadcrumbs, .partners-intro__back-btn", {
-    autoAlpha: 0,
-    duration: 1,
-  });
-  tl.from(
+  tl.fromTo(
+    ".partners-intro__breadcrumbs, .partners-intro__back-btn",
+    {
+      autoAlpha: 0,
+    },
+    {
+      autoAlpha: 1,
+      duration: 1,
+    }
+  );
+  tl.fromTo(
     ".partners-intro__heading",
     {
       autoAlpha: 0,
       duration: 1,
       y: 60,
     },
+    {
+      autoAlpha: 1,
+      y: 0,
+    },
     "-=0.5"
   );
-  tl.from(
+  tl.fromTo(
     ".cheese__slider",
     {
       autoAlpha: 0,
+      duration: 1,
+    },
+    {
+      autoAlpha: 1,
       duration: 1,
     },
     "-=0.5"
